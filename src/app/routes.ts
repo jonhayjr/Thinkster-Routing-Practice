f8891d82e94abf1eea3bcd909fb209045f3ff95b
jonhayjr <jonhayjr@gmail.com>
2022-02-01 19:36:54 -0800

    Added routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,4 +3,9 @@ import { ActorEditComponent } from './actor-edit.component';
 import { FourOhFourComponent } from './four-oh-four/four-oh-four.component'
 
 export const routingTable = [
+  {path: '', redirectTo: '/actors', pathMatch: 'full'},
+  {path: 'actors', component: ActorListComponent},
+  {path: 'actors/:id/edit', component: ActorEditComponent},
+  {path:'**', component: FourOhFourComponent}
+
 ]
